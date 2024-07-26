8550fee6bcc2fb5dba9129546780c82fcc352a96
devrrodrigues <rrodrigues.dev@outlook.com>
2024-07-26 17:20:01 -0300

    atualizações gerais

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  // Adicione outras configurações específicas do ambiente de produção aqui
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  // Adicione outras configurações específicas do ambiente de desenvolvimento aqui
+};
